0fb70be5ad6ee6343c4ebf032d391f484e34cc3a
mmuu1987 <262663941@qq.com>
2020-06-29 20:13:37 +0800

    Update OutstandingStyleFSM.cs

--- unity-photo-particle-system-master/Assets/Scripts/FSM/UIStateFSM/OutstandingStyleFSM.cs
+++ unity-photo-particle-system-master/Assets/Scripts/FSM/UIStateFSM/OutstandingStyleFSM.cs
@@ -1,11 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OutstandingStyleFSM : UIStateFSM {
+public class OutstandingStyleFSM : UIStateFSM
+{
+
+    public Button HonorListBtn;
+
+    public Button StandardList;
+
+    public Button DoubleMillion;
+
+
+    public List<Sprite> HonorTex;
+
+    public List<Sprite> StandardTex;
+
+    public List<Sprite> DoubleMillionTex;
 
 
     public OutstandingStyleFSM(Transform go) : base(go)
     {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
     }
 }
